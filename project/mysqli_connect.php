a8d410cd6c7ae764f270ca164d8c499800a071f2
CO18344 <42909454+CO18344@users.noreply.github.com>
2020-05-16 19:37:43 +0530

    Update mysqli_connect.php

--- project/mysqli_connect.php
+++ project/mysqli_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'sql306.epizy.com');
-define ('DB_USER','epiz_25784398');
-define('DB_PASSWORD', '86bE3iVmKWyTH');
-define('DB_NAME', 'epiz_25784398_CMS');
+define('DB_HOST', 'localhost');
+define ('DB_USER','root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'CMS');
 
 $dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
 OR die('Connection failed'.mysqli_connect_error());
